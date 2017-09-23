e4dcd557b9919d785b3000daeab97469206d6ce9
Joseph Sommer <jjs451@msstate.edu>
2017-09-23 16:42:32 -0500

    no

--- scraper.py
+++ scraper.py
@@ -1,11 +1,27 @@
-"""Scrapes cfbstat.com for html tables cont"""
+"""Scrapes cfbstats.com for html tables with team stats"""
+MISSISSIPPI_STATE = 430
+ALABAMA = 8
+ARKANSAS = 31
+AUBURN = 37
+FLORIDA = 235
+GEORGIA = 257
+KENTUCKY = 334
+LSU = 365
+TSUN = 433
+MISSOURI = 434
+SOUTH_CAROLINA = 648
+TENNESEE = 694
+TEXAS = 697
+VANDERBILT = 736
 
 from bs4 import BeautifulSoup 
 import requests
 import Team
 
 # gets stats from MSU's 2016 football season
-url = "http://www.cfbstats.com/2016/team/430/index.html"
+year = "2016"
+team = MISSISSIPPI_STATE
+url = "http://www.cfbstats.com/" + year + "/team/" + team + "/index.html"
 response = requests.get(url)
 data = response.text
 
